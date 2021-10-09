9e5a26c5d074d087e7ce731d59400a1ab6c98433
Anks <ankitashingre@gmail.com>
2021-10-09 23:10:58 +0530

    auth system

--- routes/web.js
+++ routes/web.js
@@ -1,6 +1,7 @@
 const authController = require('../app/http/controllers/authController')
 const homeController = require('../app/http/controllers/homeController')
 const cartController = require('../app/http/controllers/customers/cartController')
+const guest = require('../app/http/middleware/guest')
 
 function initRoutes(app) {
     
@@ -15,15 +16,19 @@ app.get('/cart', cartController().index)
    // res.render('customers/cart')
 //})
 
-app.get('/login', authController().login)
+app.get('/login',guest, authController().login)
+app.post('/login', authController().postLogin)
 //app.get('/login',(req,res)=>{
   //  res.render('auth/login')
 //})
 
-app.get('/register', authController().register)
+app.get('/register',guest, authController().register)
+app.post('/register', authController().postRegister)
 //app.get('/register',(req,res)=>{
 //    res.render('auth/register')
 //})
+
+app.post('/logout', authController().logout)
 
 app.post('/update-cart', cartController().update)
 
